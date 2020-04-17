a999789437c7de9638d7bea40806eb2aa6969c96
Senthil Kumaran <senthilkumaran.rajan@syncfusion.com>
2020-04-17 14:52:08 +0530

    WF-18006 : How to specify text alignment in WinForms ComboBox (ComboBoxAdv)?

---
+++ ComboBoxTextAlign/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ComboBoxTextAlign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxAdv1 = new Syncfusion.Windows.Forms.Tools.ComboBoxAdv();
+            this.comboBoxAdv2 = new Syncfusion.Windows.Forms.Tools.ComboBoxAdv();
+            this.comboBoxAdv3 = new Syncfusion.Windows.Forms.Tools.ComboBoxAdv();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Left";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Center";
+            // 
+            // comboBoxAdv1
+            // 
+            this.comboBoxAdv1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.comboBoxAdv1.BeforeTouchSize = new System.Drawing.Size(121, 21);
+            this.comboBoxAdv1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.comboBoxAdv1.Location = new System.Drawing.Point(203, 95);
+            this.comboBoxAdv1.Name = "comboBoxAdv1";
+            this.comboBoxAdv1.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxAdv1.Style = Syncfusion.Windows.Forms.VisualStyle.Office2016Colorful;
+            this.comboBoxAdv1.TabIndex = 2;
+            this.comboBoxAdv1.Text = "ComboBox";
+            this.comboBoxAdv1.ThemeName = "Office2016Colorful";
+            // 
+            // comboBoxAdv2
+            // 
+            this.comboBoxAdv2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.comboBoxAdv2.BeforeTouchSize = new System.Drawing.Size(121, 21);
+            this.comboBoxAdv2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.comboBoxAdv2.Location = new System.Drawing.Point(203, 157);
+            this.comboBoxAdv2.Name = "comboBoxAdv2";
+            this.comboBoxAdv2.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxAdv2.Style = Syncfusion.Windows.Forms.VisualStyle.Office2016Colorful;
+            this.comboBoxAdv2.TabIndex = 3;
+            this.comboBoxAdv2.Text = "ComboBox";
+            this.comboBoxAdv2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.comboBoxAdv2.ThemeName = "Office2016Colorful";
+            // 
+            // comboBoxAdv3
+            // 
+            this.comboBoxAdv3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.comboBoxAdv3.BeforeTouchSize = new System.Drawing.Size(121, 19);
+            this.comboBoxAdv3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.comboBoxAdv3.Location = new System.Drawing.Point(203, 220);
+            this.comboBoxAdv3.Name = "comboBoxAdv3";
+            this.comboBoxAdv3.Size = new System.Drawing.Size(121, 19);
+            this.comboBoxAdv3.Style = Syncfusion.Windows.Forms.VisualStyle.Office2016Colorful;
+            this.comboBoxAdv3.TabIndex = 5;
+            this.comboBoxAdv3.Text = "ComboBox";
+            this.comboBoxAdv3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.comboBoxAdv3.ThemeName = "Office2016Colorful";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Right";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 337);
+            this.Controls.Add(this.comboBoxAdv3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxAdv2);
+            this.Controls.Add(this.comboBoxAdv1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Alignment";
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxAdv3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Syncfusion.Windows.Forms.Tools.ComboBoxAdv comboBoxAdv1;
+        private Syncfusion.Windows.Forms.Tools.ComboBoxAdv comboBoxAdv2;
+        private Syncfusion.Windows.Forms.Tools.ComboBoxAdv comboBoxAdv3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
